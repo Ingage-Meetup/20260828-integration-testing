6064b4812cc8731226149a6d56d742f33466c0c5
Matt Kuhn <mlkkuhn@live.com>
2026-08-19 12:45:17 -0400

    solution (C#)

--- Templates/C#/IntegrationTesting/Project/User.cs
+++ Templates/C#/IntegrationTesting/Project/User.cs
@@ -9,4 +9,4 @@ public record Company(string Name, string CatchPhrase, string Bs);
 public record AddressUser(int Id, string Name, string Username, string Email, Address Address, string Phone, string Website, Company Company)
         : BaseUser(Id, Name, Username, Email, Phone, Website, Company);
 
-public record Geo(double Lat, double Lng);
+public record Geo(string Lat, string Lng);
