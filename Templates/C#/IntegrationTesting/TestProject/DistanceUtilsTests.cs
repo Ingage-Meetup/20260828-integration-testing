9b34dd2c43d39ce3917fb28c7c5d0b09e5a17551
Matt Kuhn <mlkkuhn@live.com>
2026-08-19 09:49:15 -0400

    fix two oopsies

--- Templates/C#/IntegrationTesting/TestProject/DistanceUtilsTests.cs
+++ Templates/C#/IntegrationTesting/TestProject/DistanceUtilsTests.cs
@@ -8,6 +8,6 @@ public class DistanceUtilsTests
     public void Test_CalculateDistance()
     {
         double actual = DistanceUtils.CalculateDistance(-37.3159, 81.1496, -31.8129, 63.5342);
-        Assert.That((int)actual, Is.LessThan(1200));
+        Assert.That(actual, Is.LessThan(1800));
     }
 }
