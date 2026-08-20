7a80ed99474d5ed61982d36eefe2b4373e9c59c7
Matt Kuhn <mlkkuhn@live.com>
2026-08-20 06:23:41 -0400

    fix test again

--- Templates/C#/IntegrationTesting/TestProject/DistanceUtilsTests.cs
+++ Templates/C#/IntegrationTesting/TestProject/DistanceUtilsTests.cs
@@ -8,6 +8,6 @@ public class DistanceUtilsTests
     public void Test_CalculateDistance()
     {
         double actual = DistanceUtils.CalculateDistance(-37.3159, 81.1496, -31.8129, 63.5342);
-        Assert.That(actual, Is.EqualTo(1071.0980280170143d));
+        Assert.That(actual, Is.EqualTo(1071.0980280170145d));
     }
 }
